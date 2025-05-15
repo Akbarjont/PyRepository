2510ef5bd27b8ed7a108804e0ab8c85d626093b2
Akbarjont <15akbarjon@gmail.com>
2025-05-15 17:32:27 +0500

    Create introduction.py

---
+++ lesson-1/homework/introduction.py
@@ -0,0 +1,32 @@
+task 1.
+a=5
+perimeter = 4*a
+area = a**2
+print(perimeter)
+print(area)
+
+task 2.
+import math 
+diameter = 4
+radius = diameter / 2
+length_of_circle = 2 * radius * math.pi
+print(length_of_circle)
+
+task 3.
+#Given two numbers a and b. Find their mean.
+import statistics
+a = 3
+b = 4
+mean = statistics.mean([a,b])
+print(mean)
+
+task 4.
+#Given two numbers a and b. Find their sum, product and square of each number.
+
+a = 6.0
+b = 7.0
+total = sum([a,b])
+productAB = math.prod([a,b])
+squareOfA = a**2
+squareOfB = b**2
+print(total, productAB, squareOfA, squareOfB)
